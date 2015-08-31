ac2337f168e453ec9ec713e1b23ee87149647de3
v.la <v.la@live.cn>
2015-08-31 17:34:31 +0800

    update version

--- nuget-push.cmd
+++ nuget-push.cmd
@@ -1,5 +1,5 @@
 set ng="%~dp0Tools\NuGet\NuGet.exe"
-%ng% push %~dp0artifacts\Es.Serializer.0.0.1.nupkg
-%ng% push %~dp0artifacts\Es.Serializer.Jil.0.0.1.nupkg
-%ng% push %~dp0artifacts\Es.Serializer.JsonNet.0.0.1.nupkg
-%ng% push %~dp0artifacts\Es.Serializer.ProtoBuf.0.0.1.nupkg
+%ng% push %~dp0artifacts\Es.Serializer.0.0.2.nupkg
+%ng% push %~dp0artifacts\Es.Serializer.Jil.0.0.2.nupkg
+%ng% push %~dp0artifacts\Es.Serializer.JsonNet.0.0.2.nupkg
+%ng% push %~dp0artifacts\Es.Serializer.ProtoBuf.0.0.2.nupkg
